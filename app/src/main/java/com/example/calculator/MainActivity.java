906a4402c3d92f19afc92aa74e2205b882ee7770
Nate <natej1@live.com>
2020-02-23 20:35:17 -0500

    Added unit conversion class and some mode button stuff

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 //import android.support.*;
+import android.speech.tts.TextToSpeech;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int UNIT_SELECTION = 1;
 
     int lengthUnit = 1;
+    UnitsConverter.CalculatorMode mode = UnitsConverter.CalculatorMode.Length;
 
 
 
@@ -49,16 +51,36 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView title = findViewById(R.id.title);
 
+        final TextView fromUnit = findViewById(R.id.FromUnit);
+        final TextView toUnit = findViewById(R.id.ToUnit);
+
         final Button CalculateButton = (Button) findViewById(R.id.CalculateButton);
         final Button ClearButton = (Button) findViewById(R.id.ClearButton);
         final Button ModeButton = (Button) findViewById(R.id.ModeButton);
+
+        CalculateButton.setOnClickListener(v -> {
+
+        });
 
         ClearButton.setOnClickListener(v -> {
             FromText.setText("");
             ToText.setText("");
         });
 
+        ModeButton.setOnClickListener(v -> {
+            if(mode == UnitsConverter.CalculatorMode.Length) {
+                mode = UnitsConverter.CalculatorMode.Volume;
+                fromUnit.setText("Gallons");
+                toUnit.setText("Liters");
 
+            }
+            else if(mode == UnitsConverter.CalculatorMode.Volume){
+                mode = UnitsConverter.CalculatorMode.Length;
+                fromUnit.setText("Meters");
+                toUnit.setText("Yards");
+            }
+
+        });
 
 
 
